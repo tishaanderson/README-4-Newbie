b19beaffd9db34cee67f19e8cbae63b2092c90fc
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2023-11-01 01:43:41 -0500

    INSTALL INQUIRER PACKAGE
    -package-lock.json file added along with inquirer package
    -Also, added my own TODO list in the index.js file based off ACCEPTANCE CRITERIA
    -All TODOs will be deleted at time of final project

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+
+// TODO create a command-line application that accepts user input
+// TODO user is asked a series of questions that the user answers regarding their application repo
+
+// TODO once the questions are answered, a README.md is created that displays the title of user project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+// TODO the first question the user will input is "What is the project title?"
+// TODO the next questions will be: "What is the description of the project?" "List installation instructions:" "Include Usage Information including User Story and Acceptance Criteria" "Include Contribution Guidelines" "Include Test Instructions"
+// TODO add this information to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
+// TODO have the user choose a license for the application from a list of options
+// TODO create a badge for that license to be added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
+// TODO create a prompt for the user to enter their GITHUB USERNAME
+// TODO add the username to the section of the README entitled Questions, with a link to the GitHub profile
+
+// TODO create a prompt for the user to enter their email address
+// TODO add the email to the section of the README entitled Questions, with instructions on how to reach them with additional questions
+
+// TODO make the TABLE OF CONTENTS bullets links so the user can click on them and be taken to the corresponding section of the README`
+
+
+
+
+
+
+
+
+
+
 // TODO: Include packages needed for this application
 
+const fs = require('fs');
+const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
+
 const questions = [];
+console.log(questions);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
