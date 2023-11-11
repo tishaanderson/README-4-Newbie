b933dfea3c32cdc8d4fe8322540e94427d64bc8d
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2023-11-10 23:40:47 -0600

    REMOVE PERSONAL TODO'S AND NOTES IN JS FILES
    - I removed all the personal TODOs that I create at the beginning of the project to follow. Removed from both JS files

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+
+
 function renderLicenseBadge(license) {
   let badgeURL = "";
 
@@ -38,6 +40,8 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+
+
 function renderLicenseSection(license) {
 
   const licenseBadge = renderLicenseBadge(license);
